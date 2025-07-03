6d939d325ffc6d9b92b8a876f0d9954c6cf9c50c
yalex-kim <yunbae.alex.kim@gmail.com>
2025-07-03 15:43:17 +0900

    feature/Implement read 5 times

--- DeviceDriver/device_driver.cpp
+++ DeviceDriver/device_driver.cpp
@@ -1,3 +1,4 @@
+#include <stdexcept>
 #include "device_driver.h"
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
@@ -6,8 +7,15 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+	int firstData = (int)(m_hardware->read(address));
+	for (int i = 0; i < 5; ++i) {
+		int data = (int)(m_hardware->read(address));
+		if (data != firstData) {
+			throw std::runtime_error("readFailException");
+		}
+	}
+	// If the data is consistent, return the first read value
+    return firstData;
 }
 
 void DeviceDriver::write(long address, int data)
--- DeviceDriver/main.cpp
+++ DeviceDriver/main.cpp
@@ -1,5 +1,8 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
+#include <stdexcept>
+
+using namespace testing;
 
 class MockFlashMemoryDevice : public FlashMemoryDevice {
 	public:
@@ -7,15 +10,34 @@ class MockFlashMemoryDevice : public FlashMemoryDevice {
 	MOCK_METHOD(void, write, (long address, unsigned char data), ());
 };
 
-TEST(DeviceDriver, ReadFromHW) {
+
+TEST(DeviceDriver, ReadFromHWFailException) {
 	MockFlashMemoryDevice hardware;
 	DeviceDriver driver{ &hardware };
 
-	EXPECT_CALL(hardware, read(0xFF))
-		.WillOnce(testing::Return(0xAB));
+	EXPECT_CALL(hardware, read(_))
+		.WillOnce(Return(0xAB))
+		.WillRepeatedly(Return(0xAC));
 
-	int data = driver.read(0xFF);
-	EXPECT_EQ(0xAB, data);
+	// custom exception type is expected : readFailException
+	EXPECT_THROW(driver.read(0xFF), std::runtime_error);
+}
+
+TEST(DeviceDriver, ReadFromHWPass) {
+	MockFlashMemoryDevice hardware;
+	DeviceDriver driver{ &hardware };
+
+	EXPECT_CALL(hardware, read(_))
+		.WillRepeatedly(Return(0xAB));
+
+	try {
+		int data = driver.read(0xFF);
+		EXPECT_EQ(0xAB, data);
+		
+	} 
+	catch (const std::exception& e) {
+		FAIL() << "Exception thrown: " << e.what();
+	}
 }
 
 int main() {
